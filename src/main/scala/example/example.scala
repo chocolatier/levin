73bde5bc40716b2dfa1886e879669ef33abb01ca
chocolatier <chocolatier@users.noreply.github.com>
2018-01-30 12:33:45 +0530

    Included assert in output

--- src/main/scala/example/example.scala
+++ src/main/scala/example/example.scala
@@ -17,7 +17,7 @@ object Example {
   while (cmd != null){
     cmd match {
             case Assert (term) => {
-              fw.write(Simplifications.removeExt(term).toString)
+              fw.write(Assert (Simplifications.removeExt(term)).toString)
               }
             case _ => fw.write(cmd.toString)
           }
