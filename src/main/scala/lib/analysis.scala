ea870dc20f120ed0feab1d44357516c5b22a4112
chocolatier <chocolatier@users.noreply.github.com>
2018-02-27 08:13:23 +1100

    Unapply fcns

--- src/main/scala/lib/analysis.scala
+++ src/main/scala/lib/analysis.scala
@@ -7,4 +7,15 @@ import theories.Core._
 
 package object analysis {
 
+    // Generate trees to classify 
+
+    // Returns the name of the applied function and the terms it is applied to. 
+    def unapplyFunction (t : Term) {
+        t match {
+            case  FunctionApplication (QualifiedIdentifier(Identifier(SSymbol(str),_),_), terms) => (str, terms) 
+            case _ => ("ERROR", List()) //TODO : Replace with proper exception.
+        }
+    }
+
+    
 }
