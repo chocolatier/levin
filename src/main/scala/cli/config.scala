7d1fe50828a1c4f36821faf5408fcac889659606
chocolatier <chocolatier@users.noreply.github.com>
2018-05-11 11:53:03 +1000

    loading config (gross)

--- src/main/scala/cli/config.scala
+++ src/main/scala/cli/config.scala
@@ -5,3 +5,9 @@ case class LevinConfig(
   binaryLocation: String,
   testFile: String = "testFile"
 )
+
+package object levin  {
+  var SMT2ConstraintDir = ""
+  var BinaryLocation = ""
+  var TestFile = ""
+}
--- src/main/scala/cli/main.scala
+++ src/main/scala/cli/main.scala
@@ -3,6 +3,14 @@ package levin
 object cli {
   def main(args: Array[String]) : Unit = {
     val config = pureconfig.loadConfig[LevinConfig]
-    println(config)
+    config match {
+      case Right(lC) => {
+        levin.SMT2ConstraintDir = lC.smt2ConstraintDir
+        levin.BinaryLocation = lC.binaryLocation
+        levin.TestFile = lC.testFile
+      }
+      case Left(_) => throw new Exception("Malformed Configuration File")
+    }
+
   }
 }
